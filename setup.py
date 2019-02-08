5ebe64a7d7da0dd2f9caa4ef83552116a71e13e4
math.ansang <math.ansang@gmail.com>
2019-02-09 02:58:29 +0900

    beta release

--- setup.py
+++ setup.py
@@ -4,26 +4,27 @@ from setuptools import setup, find_packages
 # x = requirements.readlines()
 # requirements .close()
 
-version = '0.1.12'
+version = '0.1.17'
 #'SomePackage>=1.0.4'
 #install_requires = [v.replace("\n", "") for v in x]
 
 install_requires = ['numpy>=1.14.5',
                     'matplotlib>=2.2.2',
                     'pandas>=0.23.3',
-                    'enum>=0.4.7']
+                    'enum>=0.4.7',
+                    'scipy >= 1.2.0']
 
 setup(
     name             = 'xenarix',
     version          = version,
     description      = 'python library for xenarix',
-    license          ='Montrix Non-Commercial License',
+    license          = 'Montrix Non-Commercial License',
     author           = 'montrix',
     author_email     = 'master@montrix.co.kr',
-    url              = 'http://www.montrix.co.kr/xenarix',
+    url              = 'https://github.com/minikie/xenarix',
     install_requires = install_requires,
-    packages         = find_packages(exclude = ['test']),
-    keywords         = ['scenario', 'finance'],
+    packages         = find_packages(exclude = ['test', 'bak']),
+    keywords         = ['scenario', 'finance', 'scenariogenerator','esg', 'alm'],
     # python_requires  = '>=2.7',
     package_data     = {
         'xenarix': ['xenarix_engine.exe'],
