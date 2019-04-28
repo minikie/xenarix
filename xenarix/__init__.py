9f415ecc290e80359b6e7740ff0e9dd8ee5abea7
math.ansang <math.ansang@gmail.com>
2019-04-29 00:06:53 +0900

    0.1.25

--- xenarix/__init__.py
+++ xenarix/__init__.py
@@ -5,7 +5,7 @@ from .calculations import *
 from .results import ResultObj
 from collections import namedtuple
 
-version = '0.1.24'
+version = '0.1.25'
 
 Corr_Item = namedtuple('Corr_Item', 'FIRST SECOND CORR')
 
